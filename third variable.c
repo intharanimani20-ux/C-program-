3c4e0382acc220d4b33b566564f7605eb1d279eb
intharanimani20-ux <intharanimani20@gmail.com>
2026-02-18 14:30:58 +0530

    Add comment for swapping numbers using a third variable
    Added a comment to describe the purpose of the program.

---
+++ third variable.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+int main() {
+    int a = 5, b = 10, temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("a = %d, b = %d", a, b);
+    return 0;
+}
+
+    printf("SI = %.2f", si);
+
+    return 0;
+}
+
